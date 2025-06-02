f79943bceafeb54522c3cb55b91fe5caef847d00
swkil <0826monica@naver.com>
2025-06-02 13:55:14 +0900

    conditional statement

---
+++ src/conditional.java
@@ -0,0 +1,13 @@
+public class conditional {
+    public static void main(String[] args) {
+        int num = 0;
+
+        if (num > 0) {
+            System.out.println("num은 0보다 크다.");
+        } else if (num < 0) {
+            System.out.println("num은 0보다 작다.");
+        } else {
+            System.out.println("num은 0이다.");
+        }
+    }
+}
