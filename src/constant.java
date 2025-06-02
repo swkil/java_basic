13d6b9c53f67f1df06f2854cbfe99fb853fbf57d
swkil <0826monica@naver.com>
2025-06-02 10:14:33 +0900

    constant_variable

---
+++ src/constant.java
@@ -0,0 +1,13 @@
+public class constant {
+    public static void main(String[] args) {
+        final int DAYS_IN_WEEK = 7; // 변하지 않는 값
+        final double PI = 3.14159;
+        final String GREETING = "Hello, world!";
+
+        System.out.println("Days in a week: " + DAYS_IN_WEEK);
+        System.out.println("Value of PI: " + PI);
+        System.out.println("Greeting: " + GREETING);
+
+//        DAYS_IN_WEEK = 9;
+    }
+}
