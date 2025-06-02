3eb30d888a4c83185f2fd1b4041f3f3b7046e079
swkil <0826monica@naver.com>
2025-06-02 11:24:27 +0900

    increment/decrement operator

--- src/operator.java
+++ src/operator.java
@@ -17,20 +17,26 @@ public class operator {
 //        int remainder = a % b;
 //        System.out.println("remainder(%): " + remainder);
 
-        a += 5; // a = a + 5;
-        System.out.println(a);
+//        a += 5; // a = a + 5;
+//        System.out.println(a);
+//
+//        b -= 1;
+//        System.out.println(b); // 2
+//
+//        b *= 5;
+//        System.out.println(b); // 10
+//
+//        b /= 2;
+//        System.out.println(b); // 5
+//
+//        b %= 3;
+//        System.out.println(b); // 2
 
-        b -= 1;
-        System.out.println(b); // 2
+        System.out.println(a++);  // 10
+        System.out.println(b--);  // 3
 
-        b *= 5;
-        System.out.println(b); // 10
-
-        b /= 2;
-        System.out.println(b); // 5
-
-        b %= 3;
-        System.out.println(b); // 2
+        System.out.println(++a);  // 12
+        System.out.println(--b);  // 1
 
     }
 }
