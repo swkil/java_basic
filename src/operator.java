157e9b26dae4d88ba7bf60518ed1fb10c4519266
swkil <0826monica@naver.com>
2025-06-02 10:29:46 +0900

    operator

---
+++ src/operator.java
@@ -0,0 +1,20 @@
+public class operator {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        int sum = a + b;
+        System.out.println("sum(+): " + sum);
+
+        int difference = a - b;
+        System.out.println("difference(-): " + difference);
+
+        int product = a * b;
+        System.out.println("product(*): " + product);
+
+        int quotient = a / b;
+        System.out.println("quotient(/): " + quotient);
+
+        int remainder = a % b;
+        System.out.println("remainder(%): " + remainder);
+    }
+}
