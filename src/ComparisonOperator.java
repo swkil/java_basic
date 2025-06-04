aa25e90954f72a5b27e65d70b4889a2602b08100
swkil <0826monica@naver.com>
2025-06-04 10:28:48 +0900

    ComparisonOperator

---
+++ src/ComparisonOperator.java
@@ -0,0 +1,14 @@
+public class ComparisonOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= 10 : " + (a >= 10));
+        System.out.println("b <= 5 : " + (b <= 5));
+    }
+}
