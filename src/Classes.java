03168d01aa7cb12a9bee9fd53bf0550f0c795aa0
swkil <0826monica@naver.com>
2025-06-10 10:49:37 +0900

    class

---
+++ src/Classes.java
@@ -0,0 +1,46 @@
+class Person {
+    String name;
+    int age;
+    String mbti;
+
+    public void sayHello() {
+        System.out.println("안녕하세요? 저는 " + name + "입니다.");
+        System.out.println("저의 나이는 " + age + "살 입니다.");
+        System.out.println("저의 MBTI는 " + mbti.toUpperCase() + "입니다.");
+    }
+
+    public void work() {
+        System.out.println("일하는 중...");
+    }
+}
+
+class Car {
+    String brand;
+    int year;
+    String color;
+
+    public void displayInfo() {
+        System.out.println("이 차의 브랜드는 " + brand + "입니다.");
+        System.out.println("이 차의 연식은 " + year + "년식 입니다.");
+        System.out.println("이 차의 색상은 " + color + "색 입니다.");
+    }
+}
+
+public class Classes {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Alice";
+        person.age = 20;
+        person.mbti = "enfp";
+
+        person.sayHello();
+        person.work();
+
+        Car car = new Car();
+        car.brand = "포르쉐";
+        car.year = 2020;
+        car.color = "빨강";
+
+        car.displayInfo();
+    }
+}
