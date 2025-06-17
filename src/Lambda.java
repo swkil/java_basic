a04e7f12b7c062c4828e35402d6946481704dbd8
swkil <0826monica@naver.com>
2025-06-17 10:38:51 +0900

    lambda

---
+++ src/Lambda.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
+}
+
+public class Lambda {
+    public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (a, b) -> a * b;
+
+        System.out.println(add.operate(2, 4));
+        System.out.println(multiply.operate(3, 5));
+
+        String[] name = {"Alice", "Bob", "Charlie"};
+
+//        for (String n : name) {
+//            System.out.println(n);
+//        }
+
+        Arrays.stream(name).forEach(n -> System.out.println(name));
+        Arrays.stream(name).forEach(System.out::println);
+    }
+}
