cf6ec3a0f1ecf1a84d3d9de29bf9ed78996c382d
swkil <0826monica@naver.com>
2025-06-20 09:17:43 +0900

    outer2

--- src/Outer.java
+++ src/Outer.java
@@ -39,10 +39,15 @@ class OutSecret {
 
 public class Outer {
     private String message = "Hello";
+    private String data = "Outer data";
 
     class Inner {
         void displayMessage() {
             System.out.println(message);
+        }
+
+        void printData() {
+            System.out.println(data);
         }
     }
 
@@ -51,9 +56,9 @@ public class Outer {
         inner.displayMessage();
     }
 
-    public Inner createInner() {
-        return new Inner();
-    }
+    public Inner createInner() {     // 반환타입이 Inner 여서
+        return new Inner();          // return 값으로 new Inner(); 가능
+    }                                // 즉, 메소드 호출로 Inner 클래스 인스턴스를 만드는 메소드
 
     public static void main(String[] args) {
         Outer outer = new Outer();
@@ -64,16 +69,26 @@ public class Outer {
 
         inner = outer.createInner();
         inner.displayMessage();
-        outer = null;
-        inner = null;
+        // outer = null;
+        // inner = null;
 
-        System.gc();
-        System.out.println("Outer와 Inner는 더 이상 사용X");
+        // System.gc();
+        // System.out.println("Outer와 Inner는 더 이상 사용X");
 
         Car2 car = new Car2("빠른 자동차");
         car.startEngine();
 
         Car2.StaticInner staticInner = new Car2.StaticInner();
         staticInner.hello();
+
+        Outer.Inner inner1 = outer.createInner(); // Inner 클래스 인스턴스를 만드는 메소드
+        // outer.new Inner(); 로 대체 가능
+        inner1.printData();
+        outer = null;                           // outer 메모리를 날려도
+        inner1.printData();                      // 실행 됨
+        inner1 = null;                           // inner 메모리 날리면
+//      inner1.printData();                      // 실행 안됨
+        System.gc();                            // 가비지 컬렉터 호출 요청
+        System.out.println("Outer와 Inner는 더 이상 사용되지 않음");
     }
 }
