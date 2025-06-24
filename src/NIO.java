a4d4995ca6f106e8d39fa0f7b23ea8f9fe682309
swkil <0826monica@naver.com>
2025-06-24 17:48:28 +0900

    new I/O

---
+++ src/NIO.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+
+public class NIO {
+    public static void writeFile(String filename, String content) {
+        Path filePath = Paths.get(filename);
+
+        try (FileChannel writeChannel = FileChannel.open(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE)){
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            buffer.put(content.getBytes());
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("파일을 성공적으로 작성했습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFile(String filename) {
+        Path filePath = Paths.get(filename);
+
+        try (FileChannel readChannel = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int bytesRead = readChannel.read(buffer);
+
+            while (bytesRead != -1) {
+                buffer.flip();
+
+                String chunk = StandardCharsets.UTF_8.decode(buffer).toString();
+                System.out.println(chunk)
+                ;
+                buffer.clear();
+                bytesRead = readChannel.read(buffer);
+            }
+            System.out.println("\n파일 읽기가 완료 되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //writeFile("dinner-menu.txt", "오늘의 저녁 메뉴는?");
+        readFile("dinner-menu.txt");
+    }
+}
